35d855d24adfb289991deca629517b2d34e66190
jross365 <jross365github@gmail.com>
2022-05-29 12:43:07 -0400

    Decent draft version of helper function

--- WorkBench.ps1
+++ WorkBench.ps1
@@ -1,159 +1,5 @@
 
     #WorkBench.ps1 is for cleaning up/testing changes to functions and blocks of code
-    
-   
-function Get-Folders {
-    [CmdletBinding()] 
-    param( 
-        [Parameter(Mandatory=$False)] [string]$Directory=((Get-Location).ProviderPath), 
-        [Parameter(Mandatory=$False)] [switch]$SuppressErrors,
-        [Parameter(Mandatory=$False)] [switch]$Recurse,
-        [Parameter(Mandatory=$False)] [switch]$NoSort,
-        [Parameter(Mandatory=$False)] [switch]$IgnoreExclusions,
-        [Parameter(Mandatory=$False)] [switch]$IncludeRoot
-        
-    )
-
-#region Define preliminary variables
-$Exclusions = 'filehistory|windows|recycle|@'
-$Dirs = New-Object System.Collections.ArrayList
-
-$EnumDirs = {[System.IO.Directory]::EnumerateDirectories("$Dir","*","TopDirectory")}
-
-#endregion
-
-#region Validate parameters
-If ($Directory.Length -eq 0){$Directory = (Get-Location).ProviderPath}
-Else {If (!(Test-Path $Directory)){throw "$Directory is not a valid path"}}
-
-If (!($IgnoreExclusions.IsPresent)){
-    
-    If ($Directory -match $Exclusions){throw "Path $Directory contains an excluded string. Please use the -IgnoreExclusions parameter"}
-
-    $NotTheseNames = {$_ -inotmatch $Exclusions}
-
-}
-
-Else {$NotTheseNames = {$_ -ne $null}} #Have to put something here, or the .Where statements break
-
-If ($IncludeRoot.IsPresent){$Dirs.Add($Directory) | out-null}
-
-#endregion
-
-$Dir = $Directory #Set for the "root" level enumeration
-
-Try {(&$EnumDirs).Where($NotTheseNames).ForEach({$Dirs.Add($_) | Out-Null})}
-catch {throw "Unable to enumerate directories in $Directory"}
-
-If ($Recurse.IsPresent){
-    
-    $DirCount = $Dirs.Count
-
-    $SubDirs = $Dirs
-    
-    Do {
-    
-        $DirQueue = New-Object System.Collections.ArrayList
-
-        :DirLoop Foreach ($Dir in $SubDirs){
-            
-            Try {(&$EnumDirs).Where($NotTheseNames).ForEach({$DirQueue.Add($_) | Out-Null})}
-            Catch {
-                
-                If (!$SuppressErrors.IsPresent){Write-Error "$($Error[0].Exception.Message.Split(':',2)[1].Trim())"}
-                Continue DirLoop
-            
-            } #Close Catch
-            
-        } #Close ForEach $Dir
-    
-        $DirQueue.ForEach({$Dirs.Add($_) | Out-Null})
-
-        $SubDirs = $DirQueue
-    
-        $DirCount = $SubDirs.Count
-    
-    } #Close Do
-    
-    Until ($DirCount -eq 0)
-
-            } #Close If $Recurse.IsPresent
-
-switch ($NoSort.IsPresent){
-
-    $True {return $Dirs}
-
-    $False {return ($Dirs | Sort-Object)}
-
-} #CloseSwitch
-
-        }
-
-        Function Get-Files ($Directory,[switch]$ExcludeFullPath){
-
-            Try {$Files = [System.IO.Directory]::EnumerateFiles("$Directory","*.*","TopDirectoryOnly")}
-            Catch {throw "$($_.Exception.Message)"} #Changed this because we're not leaning on recursion in this case
-            
-            $FilesNormalized = New-Object System.Collections.ArrayList
-            $Files.Where({$_ -notmatch 'thumbs.db'}).ForEach({$FilesNormalized.Add($_) | Out-Null})
-            
-            If ($ExcludeFullPath.IsPresent -and $FilesNormalized.Count -ne 0){ 
-                
-                (0..($FilesNormalized.Count - 1 )).ForEach({
-                    
-                    $FilesNormalized[$_] = $FilesNormalized[$_].Replace("$Directory",'').TrimStart('\\')
-                
-                }) 
-            
-            }
-            
-            Return $FilesNormalized
-            
-            }
-
-
-              <#
-        .SYNOPSIS
-        Adds a file name extension to a supplied name.
-
-        .DESCRIPTION
-        Adds a file name extension to a supplied name.
-        Takes any strings for the file name or extension.
-
-        .PARAMETER Name
-        Specifies the file name.
-
-        .PARAMETER Extension
-        Specifies the extension. "Txt" is the default.
-
-        .INPUTS
-        None. You cannot pipe objects to Add-Extension.
-
-        .OUTPUTS
-        System.String. Add-Extension returns a string with the extension or file name.
-
-        .EXAMPLE
-        PS> extension -name "File"
-        File.txt
-
-        .EXAMPLE
-        PS> extension -name "File" -extension "doc"
-        File.doc
-
-        .EXAMPLE
-        PS> extension "File" "doc"
-        File.doc
-
-        .LINK
-        Online version: http://www.fabrikam.com/extension.html
-
-        .LINK
-        Set-Item
-    #>
-
-
-
-
 
 
 
@@ -174,13 +20,15 @@ function New-AttrsHelperFile {
     param( 
         [Parameter(Mandatory=$True)] [string]$Folder, 
         [Parameter(Mandatory=$True)] [string]$SaveAs,
-        [Parameter(Mandatory=$False)] [switch]$WriteProgress
+        [Parameter(Mandatory=$False)] [switch]$WriteProgress,
+        [Parameter(Mandatory=$False)] [switch]$BreakBeep
     )
 
 begin {
-#Define and check variables
+#region Define and check variables
 If (!(Test-Path $Folder)){throw "$Folder is not a valid path"}
 
+$Results = New-Object System.Collections.ArrayList
 $ShellObj = New-Object -ComObject Shell.Application
 $Data = New-Object System.Collections.ArrayList
 
@@ -192,40 +40,42 @@ $ReclaimMemory = {
 
 } #Close Scriptblock ReclaimMemory
 
-$CSVFiles = get-childitem $Folder *.csv
+$Beep = {[system.console]::Beep()}
+
+$WrProg = {$WriteProgress.IsPresent -eq $true}
+
+$CSVFiles = Get-Files -Directory $Folder -Filter .csv
 $CSVCount = $CSVFiles.Count
 
-}
+If ($CSVCount -eq 0){throw "No CSVs were found in $Folder"}
 
-process {
+If (Test-Path $SaveAs){throw "$SaveAs already exists, please specify a new and unique file name"}
 
-}
-
-end {
-
-}
-
-
-$CSVFiles = get-childitem $Folder *.csv
-$CSVCount = $CSVFiles.Count
-
+#define counters for top-level loops:
 $w = 1
+$x = 1
+
+#endregion
+
+#region import CSVs, group results, clean up memory
 
 $CSVFiles.ForEach({
     
     $CSV = $_
     
-    Write-Progress -Activity "Importing CSVs" -Status "Getting $($CSV.Name) | $($CSVCount) entries" -Id 1 -PercentComplete ([int](($w / $CSVCount) * 100))    
+    If (&$WrProg){Write-Progress -Activity "Importing CSVs" -Status "Getting $CSV | $CSVCount entries" -Id 1 -PercentComplete ([int](($w / $CSVCount) * 100))}
     
-    (Import-csv ($CSV.FullName)).ForEach({$Data.Add($_) | out-null})
+    (Import-csv ($CSV)).ForEach({$Data.Add($_) | out-null})
     
     $w++
 
 })
 
+If (&$WrProg){Write-Progress -Activity "Importing CSVs" -Status "Completed" -Id 1 -Completed}
+
 $Extensions = $Data | Group-Object 'File extension'
 
-Remove-Variable Data
+Remove-Variable CSV,Data,w -ErrorAction SilentlyContinue
 &$ReclaimMemory
 
 #endregion
@@ -258,134 +108,161 @@ $NoValueAttrs = @(2,29,57,61,169,192,196,202,254,295)
 
 $ValidColumns.ForEach({$ReverseAttrHash.Add(($AttrHash.$_),($_))})
 
-$Results = New-Object System.Collections.ArrayList
-
 $ExtCount = $Extensions.Count
-$x = 1
-
-#endregion
-
-Foreach ($Extension in $Extensions){
-
-$ExtData = $Extension.Group
-$PropertiesHash = @{}
-$TotalProps = @{} 
-
-$FileCount = $ExtData.Count
-$y = 1
-
-Write-Progress -Activity "Analyzing Extensions" -Status "Working on $($Extension.Name) | $($ExtData.Count) entries" -Id 1 -PercentComplete ([int](($x / $ExtCount) * 100))
-
-#region determine unique properties
-Foreach ($Line in $ExtData){
-
-    Write-Progress -Activity "Scanning for unique property names" -Status "$($Line.Name)" -ParentId 1 -PercentComplete ([int](($y / $FileCount) * 100))
-
-    ($Line.psobject.Properties.Name).ForEach({
-        
-        $Property = $_
-    
-        If ($null -eq ($TotalProps."$Property")){$TotalProps.Add("$Property",0)}
-  
-        })
-
-        $y++
-   
-    }
-    
-    $TotalPropsCount = $TotalProps.Count
-    
-    Remove-Variable TotalProps; &$ReclaimMemory
-
-#endregion    
-
-#region Inventory all properties in use by files with this extension
-
-$y = 1
-
-$ExtData.ForEach({
-    
-    Write-Progress -Activity "Checking property values" -Status "$($_.Name)" -ParentId 1 -PercentComplete ([int](($y / $FileCount) * 100))
-    
-    $ObjProperties = $_.psobject.Properties.Name
-  
-    :IndexLoop Foreach ($Index in (0..$ObjProperties.GetUpperBound(0))){
-  
-        $Property = $ObjProperties[$Index]
-        
-        Try{[System.Collections.arraylist]$PropertyHashValues = $PropertiesHash."$Property"}
-        Catch {} #Suppress "Length" named property error
-        
-          If ($null -eq $PropertyHashValues){$PropertiesHash.Add($Property,[system.collections.arraylist]@($Index))}
-          
-          Else{
-            
-            $PropertyHashValues.Add($Index) | Out-Null
-            
-            $SortedValues = New-Object System.Collections.ArrayList
-            ($PropertyHashValues | Sort-Object -Unique -Descending).ForEach({$SortedValues.Add($_) | out-null})
-            
-            $PropertiesHash.Remove($Property) | out-null
-  
-            $PropertiesHash.Add($Property,$SortedValues)
-  
-          }
-  
-          If ($PropertiesHash.Count -eq $TotalPropsCount){break IndexLoop} #As soon as we've encountered every possible property, break
-  
-        } 
-
-        $y++
-
-    })
-  
-    $PropertyArrangement = New-Object System.Collections.ArrayList
-  
-    ($PropertiesHash.Keys).foreach({$PropertyArrangement.Add([pscustomobject]@{"Name" = $_; "Index" = $(($PropertiesHash.$_)[0])}) | Out-Null })
-  
-    $AllProperties = ($PropertyArrangement | Sort-Object "Index").Name
-  
-    $UsedProperties = New-Object System.Collections.ArrayList
-  
-    Remove-Variable PropertiesHash,TotalPropsCount,PropertyArrangement -ErrorAction SilentlyContinue
-    &$ReclaimMemory
-  
-    $AllProperties.ForEach({
-  
-    $Property = $_
-  
-    If (($ExtData.Where({$_."$Property".Length -ne 0})).Count -gt 0){$UsedProperties.Add($Property) | Out-Null}
-        
-    })
-
-#endregion
-
-#region Look up Attribute numbers from names, filter out "NoValueAttrs"
-
-$UsedPropertyNos = New-Object System.Collections.ArrayList
-$UsedProperties.ForEach({$UsedPropertyNos.Add($ReverseAttrHash."$_") | Out-Null})
-$NoValueAttrs.ForEach({$Attr = $_; $UsedPropertyNos = $UsedPropertyNos.Where({$_ -ne $Attr})})
-
-
-$Object = New-Object System.Object
-$Object | Add-Member -MemberType Noteproperty -Name "Extension" -Value "$($Extension.Name)"
-$Object | Add-Member -MemberType NoteProperty -Name "Attrs" -Value ($UsedPropertyNos)
-$Results.Add($Object) | out-null
-
-
-$Extensions = $Extensions.Where({$_.Name -ne ($Extension.Name)})
-
-Remove-Variable PropertiesHash,TotalPropsCount,PropertyArrangement -ErrorAction SilentlyContinue
-
-  &$ReclaimMemory
-
-$x++
 
 #endregion
 
 }
 
-$Results | ConvertTo-Json | out-file "exthelper.json"
+process {
+
+    Foreach ($Extension in $Extensions){
+
+        #region grab or define prerequisite inputs/outputs
+        $ExtData = $Extension.Group
+        $PropertiesHash = @{}
+        $TotalProps = @{} 
+        
+        $FileCount = $ExtData.Count
+        
+        If (&$WrProg){Write-Progress -Activity "Analyzing Extensions" -Status "Working on $($Extension.Name) | $($ExtData.Count) entries" -Id 1 -PercentComplete ([int](($x / $ExtCount) * 100))}
+        
+        #endregion
+
+        #region determine unique properties
+        Foreach ($Line in $ExtData){
+        
+            If (&$WrProg){Write-Progress -Activity "Scanning for unique property names" -Status "$($Line.Name)" -ParentId 1 -PercentComplete ([int](($y / $FileCount) * 100))}
+        
+            ($Line.psobject.Properties.Name).ForEach({
+                
+                $Property = $_
+            
+                If ($null -eq ($TotalProps."$Property")){$TotalProps.Add("$Property",0)}
+          
+                })
+        
+                $y++
+           
+            }
+            
+            $TotalPropsCount = $TotalProps.Count
+            
+            Remove-Variable TotalProps; &$ReclaimMemory
+        
+        #endregion    
+        
+        #region Inventory all properties in use by files with this extension
+        
+        $y = 1
+        
+        $ExtData.ForEach({
+            
+            If (&$WrProg){Write-Progress -Activity "Checking property values" -Status "$($_.Name)" -ParentId 1 -PercentComplete ([int](($y / $FileCount) * 100))}
+            
+            $ObjProperties = $_.psobject.Properties.Name
+          
+            :IndexLoop Foreach ($Index in (0..$ObjProperties.GetUpperBound(0))){
+          
+                $Property = $ObjProperties[$Index]
+                
+                Try{[System.Collections.arraylist]$PropertyHashValues = $PropertiesHash."$Property"}
+                Catch {} #Suppress "Length" named property error
+                
+                  If ($null -eq $PropertyHashValues){$PropertiesHash.Add($Property,[system.collections.arraylist]@($Index))}
+                  
+                  Else{
+                    
+                    $PropertyHashValues.Add($Index) | Out-Null
+                    
+                    $SortedValues = New-Object System.Collections.ArrayList
+                    ($PropertyHashValues | Sort-Object -Unique -Descending).ForEach({$SortedValues.Add($_) | out-null})
+                    
+                    $PropertiesHash.Remove($Property) | out-null
+          
+                    $PropertiesHash.Add($Property,$SortedValues)
+          
+                  }
+          
+                  If ($PropertiesHash.Count -eq $TotalPropsCount){If ($BreakBeep.IsPresent){&$Beep}; break IndexLoop} #As soon as we've encountered every possible property, break
+          
+                } 
+        
+                $y++
+        
+            })
+          
+            $PropertyArrangement = New-Object System.Collections.ArrayList
+          
+            ($PropertiesHash.Keys).foreach({$PropertyArrangement.Add([pscustomobject]@{"Name" = $_; "Index" = $(($PropertiesHash.$_)[0])}) | Out-Null })
+          
+            $AllProperties = ($PropertyArrangement | Sort-Object "Index").Name
+          
+            $UsedProperties = New-Object System.Collections.ArrayList
+          
+            Remove-Variable ExtData,PropertiesHash,TotalPropsCount,PropertyArrangement -ErrorAction SilentlyContinue
+            &$ReclaimMemory
+          
+            $AllProperties.ForEach({
+          
+            $Property = $_
+          
+            If (($ExtData.Where({$_."$Property".Length -ne 0})).Count -gt 0){$UsedProperties.Add($Property) | Out-Null}
+                
+            })
+        
+        #endregion
+        
+        #region Look up Attribute numbers from names, filter out "NoValueAttrs"
+        
+        $UsedPropertyNos = New-Object System.Collections.ArrayList
+        $UsedProperties.ForEach({$UsedPropertyNos.Add($ReverseAttrHash."$_") | Out-Null})
+        $NoValueAttrs.ForEach({$Attr = $_; $UsedPropertyNos = $UsedPropertyNos.Where({$_ -ne $Attr})})
+        
+        
+        $Object = New-Object System.Object
+        $Object | Add-Member -MemberType Noteproperty -Name "Extension" -Value "$($Extension.Name)"
+        $Object | Add-Member -MemberType NoteProperty -Name "Attrs" -Value ($UsedPropertyNos)
+        $Results.Add($Object) | out-null
+        
+        
+        $Extensions = $Extensions.Where({$_.Name -ne ($Extension.Name)})
+        
+        Remove-Variable AllProperties,UsedProperties,UsedPropertyNos,PropertiesHash,TotalPropsCount,PropertyArrangement -ErrorAction SilentlyContinue
+        &$ReclaimMemory
+        
+        $x++
+        
+        #endregion
+        
+        } #Close Foreach $Extension
+
+}
+
+end {
+    #region Final cleanup
+    if (&$WrProg){
+        Write-Progress -Activity "Checking property values" -Status "Complete" -ParentId 1 -Completed
+        Write-Progress -Activity "Analyzing Extensions" -Status "Complete" -Id 1 -Completed
+        }
+    Remove-Variable Object,NoValueAttrs -ErrorAction SilentlyContinue
+
+    #endregion
+
+    $Results | ConvertTo-Json | out-file "$SaveAs" -ErrorAction stop; 
+    Write-Host "Results saved as $SaveAs"
+
+    }
+
+}
 
 
-} #Close function
+
+
+
+
+
+
+
+
+
